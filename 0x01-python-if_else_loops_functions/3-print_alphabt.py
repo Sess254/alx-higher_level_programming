b8eabac7b8c746e454ac8edcb67b211bf3e7eb95
Sess254 <wamaritemoses@gmail.com>
2023-06-06 16:45:04 +0300

    alphabet soup

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for letter in range(ord('a'), ord('z') + 1):
+    if chr(letter) == 'e' or chr(letter) == 'q':
+        continue
+    else:
+        print("{:s}".format(chr(letter)), end="")
