e31ab2b6703ad00cc655df7506981b837c5ad363
Sess254 <wamaritemoses@gmail.com>
2023-07-04 14:58:24 +0300

    text identation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Defines a text identation function"""
+
+
+def text_indentation(text):
+    """Print a text with 2 new lines after chars: ., ? and :
+
+    Args:
+       str of string to print
+
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    char = 0
+    while char < len(text) and text[char] == ' ':
+        char += 1
+
+    while char < len(text):
+        print(text[char], end="")
+        if text[char] == "\n" or text[char] in ".?:":
+            if text[char] in ".?:":
+                print("\n")
+            char += 1
+            while char < len(text) and text[char] == ' ':
+                char += 1
+            continue
+        char += 1
