c93359faf52e5d7d2ee2ecd09ef1a9394f40f673
Sess254 <wamaritemoses@gmail.com>
2023-07-10 13:13:38 +0300

    same class or inherit

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines an if the obj is excatly an instance of specified class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns true if obj is an instance or of instance  class"""
+    return isinstance(obj, a_class)
