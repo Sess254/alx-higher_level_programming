276e5be41ad300a1c37fbfbd0a3451d81cb09de4
Sess254 <wamaritemoses@gmail.com>
2023-07-11 19:57:35 +0300

    student to disk reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a function that creates a student class"""
+
+
+class Student:
+    """class for student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Insitation for student attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Dictionary representation of  a student instance"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces the attributes of student"""
+        for k, v in json.items():
+            setattr(self, k, v)
