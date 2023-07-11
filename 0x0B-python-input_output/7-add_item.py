318b7809e63f40d3c04dfd00d30315caf1dae9b5
Sess254 <wamaritemoses@gmail.com>
2023-07-11 15:47:48 +0300

    fixed identation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
         data = load_from_json_file("add_item.json")
     except FileNotFoundError:
         data = []
-        data.extend(sys.argv[1:])
-        save_to_json_file(data, "add_item.json")
+    data.extend(sys.argv[1:])
+    save_to_json_file(data, "add_item.json")
