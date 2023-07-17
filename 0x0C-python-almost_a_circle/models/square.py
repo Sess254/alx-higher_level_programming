621d027f822dafb7c2e990165661a2147e635ae9
Sess254 <wamaritemoses@gmail.com>
2023-07-17 12:25:31 +0300

    and now, the square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Reps a Square that inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new square with
+           x, y and id as args
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns str rep of a square"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
