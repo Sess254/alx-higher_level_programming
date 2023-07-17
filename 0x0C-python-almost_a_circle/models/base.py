2d845c7c79ce87e0a2e85c8b559f01a68ad7481f
Sess254 <wamaritemoses@gmail.com>
2023-07-17 14:35:08 +0300

    json to string dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,10 @@ class Base:
             else:
                 list_dict = [obj.to_dictionary() for obj in list_objs]
                 jsonfile.write(Base.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of json string rep of json_string"""
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
