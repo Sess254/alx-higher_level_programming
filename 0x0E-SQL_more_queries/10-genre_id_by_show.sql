0aae21ec7aa1f71972e72f26dc48401cfad89f59
Sess254 <wamaritemoses@gmail.com>
2023-08-16 21:06:13 +0300

    genre id by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows.title, tv_shows_genres.genre.id
+SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv.shows.id = tv_show_genres.show.id
