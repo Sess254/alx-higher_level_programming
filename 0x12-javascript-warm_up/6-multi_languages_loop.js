c4cd2c8b1b3aff4ae78f5850e9a59fd4910b7fed
Sess254 <wamaritemoses@gmail.com>
2023-09-11 16:52:04 +0300

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that prints 3 lines but by using an array of string and a loop
+
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
