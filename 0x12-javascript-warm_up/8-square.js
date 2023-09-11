2c9ca6834304c3920a366655add2680476104e0a
Sess254 <wamaritemoses@gmail.com>
2023-09-11 17:32:11 +0300

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,9 @@
 
 const size = parseInt(process.argv[2]);
 
-if (!isNaN(size)) {
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
   for (let i = 0; i < size; i++) {
     let row = '';
     for (let j = 0; j < size; j++) {
@@ -11,6 +13,4 @@ if (!isNaN(size)) {
     }
     console.log(row);
   }
-} else {
-  console.log('Missing size');
 }
