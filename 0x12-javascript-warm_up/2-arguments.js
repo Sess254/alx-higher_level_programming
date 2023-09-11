81814a431fd0e905f18b10138d43d5fae9b7ccad
Sess254 <wamaritemoses@gmail.com>
2023-09-11 13:20:20 +0300

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints a message depending of the number of arguments passed
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length > 2) {
+  console.log('Arguments found');
+}
