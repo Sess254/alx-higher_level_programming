59ab69d8cbc30eb0e468262028fe540495bc33c0
Sess254 <wamaritemoses@gmail.com>
2023-09-12 19:12:45 +0300

    Factor index

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that imports an array and computes a new array
+const list = require('./100-data.js').list;
+const newList = list.map(function (num, index) {
+  return num * index;
+});
+
+console.log(list);
+console.log(newList);
