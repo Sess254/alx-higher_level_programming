6174a11a30e41af57b50fd929e387a537202d6e5
Sess254 <wamaritemoses@gmail.com>
2023-09-17 18:57:37 +0300

    Cities in state

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a python file that contains the class definition of
+   a State and an instance Base = declarative_base()
+"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class City(Base):
+    """ Class with id and name attributes of states
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
