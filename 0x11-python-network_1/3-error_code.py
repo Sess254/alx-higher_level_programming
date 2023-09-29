e60d6d60d23378016e3cc66de999263e8b789bd1
Sess254 <wamaritemoses@gmail.com>
2023-09-29 15:42:19 +0300

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
             body = response.read().decode('utf-8')
             print(body)
     except urllib.error.HTTPError as e:
-        print("Error code: ".format(e.code))
+        print("Error code: {}".format(e.code))
