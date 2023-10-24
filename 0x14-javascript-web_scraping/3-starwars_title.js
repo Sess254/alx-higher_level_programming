dd441d482fed1cff697892d8d9b3075bbd52e0b9
Sess254 <wamaritemoses@gmail.com>
2023-10-24 13:26:44 +0300

    star wars movies title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that prints the title of a Star Wars movie where the episode number
+// matches a given integer
+
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
