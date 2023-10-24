41e49d287cde40bc3e641b9a4a1553dcbb84248a
Sess254 <wamaritemoses@gmail.com>
2023-10-24 12:26:10 +0300

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', function (error, content) {
+  if (error) {
+    console.error('Error reading the file:', error);
+  } else {
+    console.log(content);
+  }
+});
